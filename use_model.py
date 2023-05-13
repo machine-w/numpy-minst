51ca072d07081fbf7e67f86c390290d50a80cc41
machine <steve2008.ma@gmail.com>
2023-05-13 11:44:10 +0800

    测试成功率

--- use_model.py
+++ use_model.py
@@ -17,9 +17,27 @@ param = pickle.load(f)
 # print(param)
 f.close
 
-test_index = np.random.randint(1000)
-data.show_test(SHOWPATH,test_index)
-predict_result = predict(data.test_img[test_index], param)
-print("predict:{}".format(predict_result.argmax()))
+# test_index = np.random.randint(1000)
+cmd = input("请输入测试图片号:")
+if cmd == "all":
+    success_num =0
+    for i in range(1000):
+        data.show_test(SHOWPATH,i)
+        predict_result = predict(data.test_img[i], param)
+        print("predict:{}".format(predict_result.argmax()))
+        # print("########################",data.test_lab[i],predict_result.argmax())
+        if str(data.test_lab[i]) == str(predict_result.argmax()):
+            success_num +=1
+    print("成功率:{}%".format(success_num/10))
+else:
+    try:
+        test_index = int(cmd)
+        data.show_test(SHOWPATH,test_index)
+        predict_result = predict(data.test_img[test_index], param)
+        print("predict:{}".format(predict_result.argmax()))
+    except Exception as e:
+        print("请输入对应数字或者all测试全部集合")
+
+
 
 
